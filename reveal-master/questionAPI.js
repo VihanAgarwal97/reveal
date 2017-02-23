f3b08bcfea3751d607dd247d410bcf29d9543300
Henry Ferguson <henrynferg@gmail.com>
2017-02-23 14:38:24 -0600

    Added param to API Requester, put results in array
    New API Parameter: questionNum. Allows you to specify how many questions you want, stores it in questionList.

--- reveal-master/questionAPI.js
+++ reveal-master/questionAPI.js
@@ -1,7 +1,7 @@
 /*This class makes calls to the question API and keeps track of already seen questions/
 
 /*Stores the URL for the API call*/
-var url = "https://opentdb.com/api.php?amount=1&difficulty=easy";
+var url = "https://opentdb.com/api.php";
 
 /*Holds all the questions already seen by the user*/
 var questions_seen = new Array();
@@ -9,8 +9,14 @@ var questions_seen = new Array();
 /*Stores the current question returned*/
 var question;
 
-/*Request the API for 1 question*/
-var requestData = function(url) {
+/*Number of squares in the main app*/
+var squareNum;
+
+/*Stores all questions to be displayed on the main app*/
+var questionList = new Array();
+
+/*Request the API*/
+var requestData = function(url, questionNum) {
         dataPromise = new Promise(function(resolve, reject) {
             $.ajax({
 
@@ -18,11 +24,12 @@ var requestData = function(url) {
                 origin: "https://opentdb.com/",
                 type: 'GET',
                 dataType: 'json',
+                data: {difficulty: 'easy', amount: questionNum},
 
                 success: function(result) {
-                    var quest = result.results[0];
-                    question = quest;
-                    resolve(question);
+                    questionList = result.results;
+                    question = questionList[0];
+                    resolve(questionList);
                 },
 
                 error: function() {
