edfd87eef1e64b125a3ba0133c6bcd73b141c97b
Henry Ferguson <henrynferg@gmail.com>
2017-02-27 13:15:44 -0600

    Re-uploaded fix for questionList

--- reveal-master/app.js
+++ reveal-master/app.js
@@ -1,7 +1,10 @@
 /**The main code of our program lies in this file. We use jQuery and pixiJS libraries.**/
 
 /*All the grid squares currently on the page*/
-var gridSquares = new Array();
+var gridSquares = {};
+
+/*Stores question data about each grid square*/
+var gridSquareQuestionData = {};
 
 /*Holds the coordatinates of the top left corner of the canvas*/
 var xPos=100;
@@ -24,7 +27,8 @@ var stage = new PIXI.Container();
 
 requestAnimationFrame(animate);
 
-/*If the questionList isn't ready yet, this function will re-run*/
+/*If the questionList isn't ready yet, this function will re-run.
+It then puts the elements on the stage.'*/
 var waiter = function() {
     if(!executed) {
         console.log("waited");
@@ -32,7 +36,11 @@ var waiter = function() {
     }
     else {
         console.log("ready");
+        console.log(questionList);
+        addPicture(url);
+        addSquares(no_grids);
     }
+    return executed;
 }
 
 /*Function that sets up the app*/
@@ -40,11 +48,7 @@ $("document").ready(function setup(){
     var imgindex=Math.floor(Math.random() * (imginfo.length));
     var thisImg=imginfo[imgindex];
     var url ="resources/" + thisImg.url;
-    addPicture(url);
     waiter();
-    console.log(questionList);
-    addSquares(no_grids);
-    
 });
 
 /*Adds a picture that needs to be guessed to the background*/
@@ -86,28 +90,33 @@ function createGridSquare(id,xPos, yPos){
     gridSquares[id].mouseout = mouseLeave_grid;
 }
 
+/*Shows the question of the clicked square*/
+function showQuestion(squareId) {
+    var squareQuestion = gridSquares[squareId].question;
+    console.log(squareQuestion);
+}
+
 /*Call to PIXI animator*/
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(stage);   
 }
 
-/*Defines a function for a grid square when clicked*/
+/*Defines a function for a grid piece when mouse up*/
 function clickEvent_grid(){
     console.log(this.question);
     stage.removeChild(this);
+    console.log("removed");
 }
 
-/*Variable to store the counter as a texture*/
 var thisTexture;
 
-/*Defines a function for a grid square when the mouse enters it*/
 function mouseEnter_grid(){
     thisTexture = this.texture;
     this.texture = new PIXI.Texture.fromImage("resources/selectedQuizBox100.png");
+    console.log("enter");
 }
-
-/*Defines a function for a grid square when the mouse leaves it*/
 function mouseLeave_grid(){
     this.texture = thisTexture;
+    console.log("left");
 }
